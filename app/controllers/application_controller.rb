0a4579fdc0ff46f5b082e512076ae11e20ce6c7d
Ian White <ian.w.white@gmail.com>
2015-12-02 12:47:41 +0000

    Adds greeting to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def current_user
+    @current_user ||= find_or_create_user
+  end
+  helper_method :current_user
+
+  def find_or_create_user
+    User.find_by(session_id: cookies[:user_session_id]) || create_user_and_store_cookie
+  end
+
+  def create_user_and_store_cookie
+    User.create!.tap do |user|
+      cookies.permanent[:user_session_id] = user.session_id
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,14 @@ class User < ActiveRecord::Base
   has_many :interactions, inverse_of: :user
 
   before_validation :ensure_name
+  before_validation :ensure_session_id
 
   def ensure_name
-    [User.adjectives.sample, User.animal_names.sample].join(" ")
+    self.name = [User.adjectives.sample, User.animal_names.sample].join(" ") if name.blank?
+  end
+
+  def ensure_session_id
+    self.session_id = SecureRandom.hex(24) if session_id.blank?
   end
 
   cattr_accessor :adjectives do
